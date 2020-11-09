b387387c16639322888587e0af4541897479638e
村长 <757891685@qq.com>
2020-11-09 18:48:16 +0800

    [新增代码]程序提供自启动功能

--- SoundAndLightAlarm/Program.cs
+++ SoundAndLightAlarm/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Diagnostics;
 using System.Linq;
 using System.Windows.Forms;
@@ -38,9 +39,42 @@ namespace SoundAndLightAlarm
             }
             #endregion
 
+            SetAutoBootStatu(true);
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new SoundAlarm(args[0]));
+        }
+
+        /// <summary>  
+        /// 在注册表中添加、删除开机自启动键值  
+        /// </summary>  
+        public static int SetAutoBootStatu(bool isAutoBoot)
+        {
+            try
+            {
+                string execPath = Application.ExecutablePath;
+                RegistryKey rk = Registry.LocalMachine;
+                RegistryKey rk2 = rk.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run");
+                if (isAutoBoot)
+                {
+                    rk2.SetValue("SoundAndLightAlarm", execPath);
+                    Console.WriteLine(string.Format("[注册表操作]添加注册表键值：path = {0}, key = {1}, value = {2} 成功", rk2.Name, "TuniuAutoboot", execPath));
+                }
+                else
+                {
+                    rk2.DeleteValue("SoundAndLightAlarm", false);
+                    Console.WriteLine(string.Format("[注册表操作]删除注册表键值：path = {0}, key = {1} 成功", rk2.Name, "TuniuAutoboot"));
+                }
+                rk2.Close();
+                rk.Close();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("[注册表操作]向注册表写开机启动信息失败, Exception: {0}", ex.Message));
+                return -1;
+            }
         }
     }
 }
